2c092e8af56f3c2c9395f932a77b8e522d6cb07c
grini_youssef <grini.youssef@gmail.com>
2020-04-06 18:56:02 +0100

    bd redis

--- vote/app.py
+++ vote/app.py
@@ -40,6 +40,10 @@ def hello():
     resp.set_cookie('voter_id', voter_id)
     return resp
 
+# @app.route('/action', methods=['POST','GET'])
+# def about():
+# 	return "tu as clique"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
